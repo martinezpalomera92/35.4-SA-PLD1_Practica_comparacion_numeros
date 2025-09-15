86917b71abd9bea84343ff04b48f57a0ab30daf6
Jaime Martínez <martinezpalomera92@gmail.com>
2025-09-15 14:01:34 -0600

    Declared variables and started comparations with if

---
+++ main.js
@@ -0,0 +1,7 @@
+let num1 = parseInt(prompt("Enter first number:"));
+let num2 = parseInt(prompt("Enter second number:"));
+let num3 = parseInt(prompt("Enter third number:"));
+
+if (num1 >= num2 && num1 >= num3) {
+    
+}
